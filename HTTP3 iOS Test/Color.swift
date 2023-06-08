21d072402ac32bee42385601f1f16bd418e6a92e
Mike <laguerdo@yahoo.com>
2023-06-08 09:30:31 +0700

    initial commit

---
+++ HTTP3 iOS Test/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  HTTP3 iOS Test
+//
+//  Created by Michael Antonius on 07/06/23.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: UInt32, alpha: CGFloat = 1.0) {
+        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(hex & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
--- HTTP3 iOS Test/ViewController.swift
+++ HTTP3 iOS Test/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import os.log
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, URLSessionDataDelegate {
+    @IBOutlet weak var protocolTxt: UILabel!
+    
+    var session: URLSession!
+    var task: URLSessionDataTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create a URLSession with a delegate
+        session = URLSession(configuration: .default, delegate: self, delegateQueue: .main)
     }
+    
+    @IBAction func refreshBtnAction(_ sender: Any) {
+        let url = URL(string: "https://quic.tech:8443/")!
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 60.0)
+        request.assumesHTTP3Capable = true
+        os_log("task will start, url: \(url.absoluteString)")
+        self.session.dataTask(with: request) { (data, response, error) in
+            if let error = error as NSError? {
+                os_log("task transport error \(error.domain) / \(error.code)")
+                return
+            }
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                os_log("task response is invalid")
+                return
+            }
 
 
+            guard 200 ..< 300 ~= response.statusCode else {
+                print("task response status code is invalid; received \(response.statusCode), but expected 2xx")
+                return
+            }
+            os_log("task finished with status \(response.statusCode), bytes \(data.count)")
+        }.resume()
+    }
+    
+    // URLSessionDelegate method to capture task metrics
+    func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
+        let protocols = metrics.transactionMetrics.map { $0.networkProtocolName ?? "-" }.joined(separator:",")
+        
+        if metrics.transactionMetrics.first != nil {
+            if(protocols.contains("h3")){
+                DispatchQueue.main.async {
+                    self.protocolTxt.backgroundColor = UIColor(hex: 0x00FF00)
+                    self.protocolTxt.textColor = UIColor(hex: 0x00000)
+                    self.protocolTxt.text = "\(protocols)"
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.protocolTxt.backgroundColor = UIColor(hex: 0xFF0000)
+                    self.protocolTxt.textColor = UIColor(hex: 0xFFFFFF)
+                    self.protocolTxt.text = "\(protocols)"
+                }
+            }
+            
+        }
+    }
 }
-
